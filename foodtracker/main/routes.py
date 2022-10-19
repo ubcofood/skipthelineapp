c907178b8e16e992dccc0b5d4f9c78d51735bbb5
Anđela Aćić <andjela.acic.2002@gmail.com>
2022-10-19 16:53:10 -0700

    initial commit

---
+++ foodtracker/main/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint,render_template 
+
+main = Blueprint('main',__name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/add')
+def add():
+    return render_template('add.html')
+
+@main.route('/view')
+def view():
+    return render_template('view.html')
