ef34868107fdd0c3987ae829f429bdf2dc0c9546
Anđela Aćić <andjela.acic.2002@gmail.com>
2022-12-05 23:19:52 -0800

    added on the budget and protein page

--- foodtracker/main/routes.py
+++ foodtracker/main/routes.py
@@ -1,7 +1,12 @@
 from flask import *
-from flask_googlemaps import GoogleMaps
-from flask_googlemaps import Map
+import googletrans 
+from googletrans import Translator
 import pyrebase
+import spoonacular as sp
+import random
+import requests
+import json
+
 
 config = {
    "apiKey" : "AIzaSyA4av3lN5gUTSXLSMuBQeigqOZuQiz5fjo",
@@ -12,6 +17,7 @@ config = {
     "messagingSenderId": "749934275221",
     "appId": "1:749934275221:web:ff9e8d5201675414ff499c"
 }
+api = sp.API('c4e983e132c1412a9ab494902242598c')
 
 firebase = pyrebase.initialize_app(config)
 
@@ -23,8 +29,37 @@ db = firebase.database()
 
 main = Blueprint('main',__name__)
 
-app = Flask(__name__, template_folder=".")
-GoogleMaps(app)
+def call_API(foodName, apiKey):
+    url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query={foodName}&dataType=Branded&pageSize=25&pageNumber=2&sortBy=dataType.keyword&sortOrder=asc&brandOwner=Kar%20Nut%20Products%20Company&api_key=gjfigRBqjrJISugOEoF7O9E45ilS2jEUK4He5gjY'
+    r = requests.get(url)
+    print(r.status_code)  # 200
+    
+    ret = r.json()
+
+    items= []
+    for i in ret['foods']:
+
+        lala = i['brandName']+" "
+        lala+=i['brandOwner']+" "
+        if('subbrandName' in i):
+            lala+=i['subbrandName']+" "
+         
+        if(len(i['foodNutrients'])>0):
+            lala+=str(i['foodNutrients'][0]['value'])+ "g protein"
+        items.append(lala)
+
+    # for i in (1,50):
+    #     print(ret[i]['description'])
+    #     print(ret[i]['brandOwner'])
+    #     print(ret[i]['foodNutrients'][0]['amount']) #protein
+    #     print(ret[i]['foodNutrients'][1]['amount']) #fat
+    #     print(ret[i]['foodNutrients'][2]['amount']) #carbs
+    #     print(ret[i]['foodNutrients'][4]['amount']) #kcal
+    print(type(ret['foods']))
+    print(len(ret['foods']))
+
+    return items
+
 
 @main.route('/', methods=['GET', 'POST']) #firebase
 def index():
@@ -39,11 +74,38 @@ def index():
         for val in foodinfo:
             if db.child("food").child(val).get().val()['foodName']=='BLT sandwich':
                db.child("carts").child(uid).push(val)
-    return render_template('index.html')
+    return render_template('index.html', randtext="lalal")
+
+@main.route('/protein', methods=['GET', 'POST'])
+def protein():
+    if request.method == 'POST':
+        foodname = request.form['searchfood']
+        ans = call_API(foodname, "gjfigRBqjrJISugOEoF7O9E45ilS2jEUK4He5gjY")
+        return render_template('protein.html',randtext=foodname, listitems= ans)
+    return render_template('protein.html')
 
 @main.route('/add')
 def add():
     return render_template('add.html')
+
+@main.route('/onthebudget')
+def budget():
+    
+
+    n = random.randint(1, 100)
+    response = api.get_recipe_information(n)
+    data = response.json()
+    ings="Ingredients: \n"
+    instructions ="Instructions: \n"
+
+    if data['extendedIngredients']!=None:
+        for ing in data['extendedIngredients']:
+            ings+=ing['original']+"\n"
+
+    if data['instructions']!=None:
+        instructions+=data['instructions'].replace(".", "\n")
+
+    return render_template('onthebudget.html', title =data['title'], link =data['image'],servings="Servings: "+str(data['servings']),preptime="Ready in "+str(data['readyInMinutes'])+" minutes", ingredients = ings, instructions=instructions)
 
 @main.route('/view')
 def view():
